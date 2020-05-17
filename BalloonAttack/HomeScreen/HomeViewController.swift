f2e10dd650108c849945dde23645d2d8712a2f20
SamiBeig <Sami.Beig45@myhunter.cuny.edu>
2020-05-16 20:01:42 -0400

    added background changes to weather screen based on weatherID

--- BalloonAttack/HomeScreen/HomeViewController.swift
+++ BalloonAttack/HomeScreen/HomeViewController.swift
@@ -11,31 +11,71 @@ import SpriteKit
 import GameplayKit
 import CoreLocation
 
-class HomeViewController: UIViewController{
-
+class HomeViewController: UIViewController, CLLocationManagerDelegate{
+  
+  let locationManager = CLLocationManager()
+  //var location: CLLocation?
+  var isUpdatingLocation = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    //self.view.backgroundColor = UIColor.red
-    //elf.view.backgroundColor = UIColor.blue
-    //view.backgroundColor = .black
     self.navigationController?.setNavigationBarHidden(true, animated: false)
-
+    
+    
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    if authorizationStatus == .notDetermined{
+      locationManager.requestWhenInUseAuthorization()
+    }
+    
+    if authorizationStatus == .denied || authorizationStatus == .restricted{
+      let alert = UIAlertController(title: "Please change your location settings for this app!",  message:"Please go to Settings -> Privary and fix this!", preferredStyle: .alert)
+      let okAction = UIAlertAction(title: "OK", style: .default, handler: nil )
+      alert.addAction(okAction)
+      
+      present(alert, animated: true, completion: nil)
+      
+    }
+    
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    locationManager.startUpdatingLocation()
+    isUpdatingLocation = true
+    
+    //refresh([])
+    
   }
+  
+  
+  @objc func didTimeOut(){
+    print("Time out")
+  }
+  
+  func locationManager(_ manager: CLLocationManager,
+                       didFailWithError error: Error) {
+    print("didFailWithError \(error.localizedDescription)")
+  }
+  func locationManager(_ manager: CLLocationManager,
+                       didUpdateLocations locations: [CLLocation]) {
+    Singleton.location = locations.last!
+    //print("didUpdateLocations \(newLocation)")
+  }
+  
+  
+  
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     self.navigationController?.setNavigationBarHidden(true, animated: false)
-
+    
   }
-
-
+  
+  
 }
 
 class Singleton {
   
   static var weatherID = 200
-    
+  static var location:CLLocation?
+  
   // Make init method private
   
   private init() {
--- BalloonAttack/HomeScreen/WeatherViewController.swift
+++ BalloonAttack/HomeScreen/WeatherViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 import CoreLocation
 
-class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+class WeatherViewController: UIViewController {
   
-  let locationManager = CLLocationManager()
-  var location: CLLocation?
-  var isUpdatingLocation = false
-  var timer:Timer!
   
   @IBOutlet weak var weatherLabel: UILabel!
-  
+  @IBOutlet weak var weather: UILabel!
   @IBOutlet weak var degreeLabel: UILabel!
   @IBOutlet weak var weatherID: UILabel!
+  @IBOutlet weak var feelsLikeLabel: UILabel!
+  @IBOutlet weak var cityLabel: UILabel!
+  
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -29,58 +29,10 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     self.navigationController?.navigationBar.barTintColor = UIColor(red: 112.0/255.0, green: 193.0/255.0, blue: 179.0/255.0, alpha: 1.0)
     weatherID.text = "Weather ID = \(Singleton.weatherID)"
     
-    let authorizationStatus = CLLocationManager.authorizationStatus()
-    if authorizationStatus == .notDetermined{
-      locationManager.requestWhenInUseAuthorization()
-    }
-    
-    if authorizationStatus == .denied || authorizationStatus == .restricted{
-      let alert = UIAlertController(title: "Please change your location settings for this app!",  message:"Please go to Settings -> Privary and fix this!", preferredStyle: .alert)
-      let okAction = UIAlertAction(title: "OK", style: .default, handler: nil )
-      alert.addAction(okAction)
-      
-      present(alert, animated: true, completion: nil)
-      
-    }
-    
-    locationManager.delegate = self
-    locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-    locationManager.startUpdatingLocation()
-    isUpdatingLocation = true
-    
-    refresh([])
-    
-  }
-  
-  @objc func didTimeOut(){
-    print("Time out")
-  }
-  
-  func locationManager(_ manager: CLLocationManager,
-                       didFailWithError error: Error) {
-    print("didFailWithError \(error.localizedDescription)")
-  }
-  func locationManager(_ manager: CLLocationManager,
-                       didUpdateLocations locations: [CLLocation]) {
-    location = locations.last!
-    //print("didUpdateLocations \(newLocation)")
-  }
-  
-  func setWeather(description: String?, temp: Int, ID: Int){
-    weatherLabel.text = description?.capitalizingFirstLetter() ?? "..."
-    degreeLabel.text = "\(temp)° F"
-    weatherID.text = "Weather ID = \(ID)"
-    
-    
-  }
-  
-  
-  @IBAction func refresh(_ sender: Any) {
-    if let location = location{
-      
+    if Singleton.location != nil{
       let urlBeginning = "https://api.openweathermap.org/data/2.5/weather?"
-      let lat = "lat=\(location.coordinate.latitude)"
-      let lon =  "&lon=\(location.coordinate.longitude)&units=imperial"
+      let lat = "lat=\(Singleton.location!.coordinate.latitude)"
+      let lon =  "&lon=\(Singleton.location! .coordinate.longitude)&units=imperial"
       let apiKey = API_KEY
       
       let temp = urlBeginning + lat + lon + apiKey
@@ -92,13 +44,17 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         if let data = data, error == nil {
           do{
             guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else{ return}
-            guard let weatherDetails = json["weather"] as? [[String: Any]], let weatherMain = json["main"] as? [String: Any] else{ return }
+            guard let weatherDetails = json["weather"] as? [[String: Any]], let weatherMain = json["main"] as? [String: Any], let country = json["name"] as? String? else{ return }
+            let weather = weatherDetails.first?["main"] as? String
             let temp = Int(weatherMain["temp"] as? Double ?? 0 )
+            let feelsLikeTemp = Int(weatherMain["feels_like"] as? Double ?? 0)
             let description = (weatherDetails.first?["description"] as? String)
             let jsonID = (weatherDetails.first?["id"] as? Int ?? 0)
+            
             DispatchQueue.main.async{
-              self.setWeather(description: description, temp:temp, ID: jsonID )
+              self.setWeather(weatherDetails: weather, countryName: country, description: description, temp:temp, feelsLikeTemp: feelsLikeTemp, ID: jsonID )
             }
+            
           }
           catch{
             print("ERROR")
@@ -109,8 +65,61 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
       }
       task.resume()
       
+      //self.changeBackground()
+    }
+    else{
+      weatherLabel.text = "Could not be found"
+      degreeLabel.text = "Could not be found"
+      
+    }
+    //print(Singleton.weatherID)
+    //changeBackground()
+    
+  }
+  
+  
+  override func viewWillAppear(_ animated: Bool) {
+    self.changeBackground()
+
+  }
+  
+  
+  
+  
+  func setWeather(weatherDetails: String?, countryName: String?, description: String?, temp: Int, feelsLikeTemp: Int, ID: Int){
+    
+    weatherLabel.text = description?.capitalizingFirstLetter() ?? "..."
+    degreeLabel.text = "\(temp)° F"
+    weatherID.text = "Weather ID = \(ID)"
+    feelsLikeLabel.text = "Feels like \(feelsLikeTemp)° F"
+    cityLabel.text = countryName
+    weather.text = weatherDetails
+    Singleton.weatherID = ID
+    
+    self.changeBackground()
+    
+    
+  }
+  
+  func changeBackground(){
+    switch Singleton.weatherID {
+    case 200...399:
+      self.view.backgroundColor = UIColor.yellow
+      
+    case 300...399:
+      self.view.backgroundColor = UIColor.systemIndigo
+      
+    case 500...599:
+      self.view.backgroundColor = UIColor.blue
+    case 600...699:
+      self.view.backgroundColor = UIColor.green
+    case 700...799:
+      self.view.backgroundColor = UIColor.orange
+    default:
+      self.view.backgroundColor = UIColor.systemGray2
     }
   }
+  
   
   
 }
